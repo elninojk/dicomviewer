eee07b92d48788ef60b86188e67b73ae28a6140b
rajavishnu-k <55690151+rajavishnu-k@users.noreply.github.com>
2019-10-08 18:04:27 +0530

    Add files via upload

---
+++ DCMFileNotFoundExcepiton.java
@@ -0,0 +1,20 @@
+package dcmparser;
+
+public class DCMFileNotFoundExcepiton extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DCMFileNotFoundExcepiton() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public DCMFileNotFoundExcepiton(String msg) {
+		super(msg);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
--- DCMParser.java
+++ DCMParser.java
@@ -1,71 +1,133 @@
 package dcmparser;
 
 import java.io.File;
-import java.io.IOException;
-import java.sql.SQLException;
 import org.dcm4che3.data.Attributes;
 import org.dcm4che3.data.Tag;
 import org.dcm4che3.io.DicomInputStream;
+import org.postgresql.util.PSQLException;
 
 import daoclasses.ImageDAO;
 import daoclasses.PatientStudyDAO;
 import daoclasses.SeriesDAO;
-import ui.home;
+import ui.Home;
+
+
 
 public class DCMParser {
 	
 	static DicomInputStream din;
 	// dicom file parser
-	public static void dcmFileParser(File f)  throws Exception
+	public static void dcmFileParser(File f) throws Exception  
 	{		
-		din = new DicomInputStream(f);				
-		Attributes tagDataSet = din.readDataset(-1, -1);	
-		String studyDateTime;
-		studyDateTime = tagDataSet.getString((int) Tag.StudyDateAndTime);
 		
-		
-		// creating new patient, study, image obj
-		PatientStudy  ps = new PatientStudy(tagDataSet.getString(Tag.PatientID), tagDataSet.getString(Tag.PatientName), tagDataSet.getString(Tag.PatientBirthDate), tagDataSet.getString(Tag.AccessionNumber), tagDataSet.getString(Tag.StudyInstanceUID), tagDataSet.getString(Tag.StudyDescription), studyDateTime);
-		Series s = new Series(tagDataSet.getString(Tag.SeriesInstanceUID), tagDataSet.getString(Tag.StudyInstanceUID), tagDataSet.getString(Tag.SeriesNumber), tagDataSet.getString(Tag.Modality), tagDataSet.getString(Tag.SeriesDescription));
-		Image i = new Image(tagDataSet.getString(Tag.ReferenceImageNumber), tagDataSet.getString(Tag.StudyInstanceUID), tagDataSet.getString(Tag.SeriesInstanceUID), tagDataSet.getString(Tag.ImageType), tagDataSet.getString(Tag.Rows), tagDataSet.getString(Tag.Columns) , tagDataSet.getString(Tag.BitsAllocated), tagDataSet.getString(Tag.BitsStored));
-		//	Adding to db
-		//	DcmDAO.insertPatient(patient); 		
-		
-		PatientStudyDAO.insert(ps);
-		SeriesDAO.insert(s);
-		ImageDAO.insert(i);
-		// 	Displaying info	
-		System.out.println("patient \n " + ps);
-		System.out.println("\nstudy \n " + s);
-		System.out.println("\nimage \n " + i);
-
-		
-		
-	}
-	// dicom folder parser
-	public static void dcmFolderParser(File[] f) throws Exception
-	{
-		for(File file : f)
-		{
-			din = new DicomInputStream(file);				
+		try
+		{   
+			din = new DicomInputStream(f);			
 			Attributes tagDataSet = din.readDataset(-1, -1);	
+			String sdate = tagDataSet.getString(Tag.StudyDate);
+			String stime = tagDataSet.getString(Tag.StudyTime);
 			
-			// adding patient, series, image info to the list
-			PatientStudy  ps = new PatientStudy(tagDataSet.getString(Tag.PatientID), tagDataSet.getString(Tag.PatientName), tagDataSet.getString(Tag.PatientBirthDate), tagDataSet.getString(Tag.AccessionNumber), tagDataSet.getString(Tag.StudyInstanceUID), tagDataSet.getString(Tag.StudyDescription), tagDataSet.getString((int) Tag.StudyDateAndTime));
-			Series s = new Series(tagDataSet.getString(Tag.SeriesInstanceUID), tagDataSet.getString(Tag.StudyInstanceUID), tagDataSet.getString(Tag.SeriesNumber), tagDataSet.getString(Tag.Modality), tagDataSet.getString(Tag.SeriesDescription));
+			StringBuffer studyDate = splitDate(sdate);			
+			StringBuffer studyTime = splitTime(stime);
+			//System.out.println(tagDataSet.getString());
+			// creating new patient, study, image obj
+			PatientStudy  ps = new PatientStudy(tagDataSet.getString(Tag.PatientID), tagDataSet.getString(Tag.PatientName), tagDataSet.getString(Tag.PatientBirthDate), tagDataSet.getString(Tag.AccessionNumber), tagDataSet.getString(Tag.StudyInstanceUID), tagDataSet.getString(Tag.StudyDescription), String.valueOf(studyDate.append("  " + studyTime)));
+			Series s = 	new Series(tagDataSet.getString(Tag.SeriesInstanceUID), tagDataSet.getString(Tag.StudyInstanceUID), tagDataSet.getString(Tag.SeriesNumber), tagDataSet.getString(Tag.Modality), tagDataSet.getString(Tag.SeriesDescription));
 			Image i = new Image(tagDataSet.getString(Tag.ReferenceImageNumber), tagDataSet.getString(Tag.StudyInstanceUID), tagDataSet.getString(Tag.SeriesInstanceUID), tagDataSet.getString(Tag.ImageType), tagDataSet.getString(Tag.Rows), tagDataSet.getString(Tag.Columns) , tagDataSet.getString(Tag.BitsAllocated), tagDataSet.getString(Tag.BitsStored));
+			//	Adding to db				
 			
 			PatientStudyDAO.insert(ps);
 			SeriesDAO.insert(s);
 			ImageDAO.insert(i);
-			
-			// Displaying info
-			System.out.println("patient \n " + ps);
-			System.out.println("\nstudy \n " + s);		
-			System.out.println("\nimage \n " + i);		
+			// 	Displaying info	
+			Home.dispPatient();
+		}
+		catch (PSQLException  e) {
+			throw e;
+		}
+		catch(NullPointerException e)
+		{
+			throw e;
+		}
+		catch (Exception e) {
+			throw e;
+		}
+
+	}
+	
+	// dicom folder parser
+	public static void dcmFolderParser(File[] f) throws Exception
+	{
+		boolean flag = false;
+		for(File file : f)
+		{
+			if(file.getName().endsWith(".dcm"))
+			{				
+				try			
+				{				
+					flag = true;				
+					dcmFileParser(file);
+				}
+				catch (PSQLException  e) {
+					throw e;
+				}
+				catch(NullPointerException e)
+				{
+					throw e;
+				}
+				catch (Exception e) {
+					throw e;
+				}
+				
+			}
+			else if(flag == false)
+			{
+				throw new DCMFileNotFoundExcepiton("DCM file not found in the selected folder");
+			}						
 			
 		}
+	
 	}	
+	
+	private static StringBuffer splitDate(String sd) {
+		StringBuffer studyDate = new StringBuffer();
+		for(int i=0; i < sd.length(); i++)
+		{
+			
+			if(i == 4 || i == 6 || i == 8) 
+			{
+				studyDate.append("/");
+			}
+			else
+			{
+				studyDate.append(sd.charAt(i));
+			}
+		}
+		return studyDate;
+		
+	}
+	
+	private static StringBuffer splitTime(String st) {
+		StringBuffer studyDate = new StringBuffer();
+		for(int i=0; i < st.length(); i++)
+		{
+			
+			if(i == 2 || i == 5) 
+			{
+				studyDate.append(":");
+			}
+			else if ( i == 8)
+			{
+				break;
+			}
+			else
+			{
+				studyDate.append(st.charAt(i));
+			}
+			
+		}	
+		return studyDate;		
+	}
 	
 	
 	
