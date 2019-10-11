252cfbe1a32eb0cd11acbbf0e03e1b2fd1d14253
rajavishnu-k <55690151+rajavishnu-k@users.noreply.github.com>
2019-10-11 15:04:37 +0530

    Add files via upload

--- DicomViewer-new/exceptions/DCMFileNotFoundExcepiton.java
+++ DicomViewer-new/exceptions/DCMFileNotFoundExcepiton.java
@@ -1,1 +1,16 @@
+package exceptions;
 
+public class DCMFileNotFoundExcepiton extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	public DCMFileNotFoundExcepiton(String msg) {
+		super(msg);		
+	}
+
+	
+}
