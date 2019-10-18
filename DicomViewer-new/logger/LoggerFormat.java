87d01d45d755e110b3be50b3381e2c9bbf20abed
rajavishnu-k <55690151+rajavishnu-k@users.noreply.github.com>
2019-10-18 11:06:33 +0530

    Add files via upload

--- DicomViewer-new/logger/LoggerFormat.java
+++ DicomViewer-new/logger/LoggerFormat.java
@@ -1,1 +1,25 @@
+package logger;
 
+import java.io.IOException;
+import java.util.Date;
+import java.util.logging.FileHandler;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+public class LoggerFormat extends Formatter{
+
+	public LoggerFormat() {
+		 
+	}
+public String format(LogRecord record) {
+        return record.getThreadID()+"::"+record.getSourceClassName()+"::"+record.getSourceMethodName()+"::"
+                +new Date(record.getMillis())+"::"
+                +record.getMessage()+"\n";
+    }
+public static FileHandler getFileHandler() throws SecurityException, IOException
+	{
+		FileHandler fHandler = new FileHandler(".//Logger.log");
+		fHandler.setFormatter(new LoggerFormat());
+		return fHandler;	
+		}
+}
